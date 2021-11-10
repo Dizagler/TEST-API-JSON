875a1f500ef71a473df5762d99f1c3c5b4fcee5c
Dizagler <sid_alex@yahoo.com>
2021-11-10 11:50:34 +0300

    321

--- assets/main.js
+++ assets/main.js
@@ -20,8 +20,8 @@ form.addEventListener('submit', (e) => {
 })
 
 socket.on('chat message', (data) => {
-  const item = document.createElement('li')
-  item.innerHTML = `<span>${data.name}</span>: ${data.message}`
-  messages.appendChild(item)
+  const item = document.createElement('li');
+  item.textContent = `<span>${data.name}</span>: ${data.message}`
+  messages.appendChild(item);
 })
 
--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const io = require("socket.io")(http);
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
-// app.use(express.static(__dirname + '/assets'))
+app.use(express.static(__dirname + '/assets'))
 
 io.on('connection', (socket) => {
    socket.on('chat message', (data) => {
-     io.emit('chat message', {
+     io.emit('chat messge', {
        message: data.message,
        name: data.name
      })
    })
 })
 
-http.listen(3000, () => {
+server.listen(3000, () => {
   console.log('Сервер стартанул')
 });
